fd9cd2c795345fe88bbf0b3c84bf1a3fe86f8ca8
zm4kpsxr4q-coder <zm4kpsxr4q@privaterelay.appleid.com>
2026-05-25 11:49:32 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/mychatapp/MyChatApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mychatapp;
+
+/**
+ *
+ * @author HLUMELO
+ */
+public class MyChatApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
